ab2ded6e5a459df5bfb0f1b37ce0408bc54fcf8b
Harris Mahmood <mahmomuh@shernet.sheridancollege.ca>
2020-10-06 15:51:22 -0400

    Renamed the insert Method name insertUser --> insertAll

--- app/src/main/java/com/example/paypark/viewmodels/UserViewModel.kt
+++ app/src/main/java/com/example/paypark/viewmodels/UserViewModel.kt
@@ -1,12 +1,12 @@
 package com.example.paypark.viewmodels
-
+//https://github.com/hurrysG/PayPark-Android.git
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import com.example.paypark.database.PayParkDatabase
 import com.example.paypark.database.UserRepository
 import com.example.paypark.model.User
-
+//
 class UserViewModel(application: Application) : AndroidViewModel(application) {
 
     private val userRepo: UserRepository
@@ -20,7 +20,7 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     /**
-     * insertUser() method will create a new user record in the databaseee
+     * insertAll() method will create a new user record in the databaseee
      */
-    fun insertUser(){}
+    fun insertAll(user : User){}
 }
